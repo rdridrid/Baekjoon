10add8444df9a43fbf3791ae3b6b64c385bd53fb
rdridrid <55067985+rdridrid@users.noreply.github.com>
2023-01-03 21:20:08 +0900

    [Bronze I] Title: 단어 공부, Time: 448 ms, Memory: 28760 KB -BaekjoonHub

---
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String word = sc.next();
+        char alpha='1';
+        char[] maxalpha = new char[26];
+        int max;
+        int flag = 0;
+        for(int j=0;j<26;j++){
+            maxalpha[j] = 0; //0으로 모두 초기화
+        }
+        for(int i=0;i<word.length();i++){
+            alpha=word.charAt(i);
+            if(alpha>'Z'){//소문자
+                maxalpha[alpha-'a']++;//a일때 0
+            }
+            else{
+                maxalpha[alpha-'A']++;//A일때 0
+            }
+        }
+        max=-1;
+        for(int k=0;k<26;k++){
+            if(max<maxalpha[k]){
+                flag = 0; //최대값을 찾으면 flag초기화(같은 빈도의 알파벳이 없음)
+                max=maxalpha[k];
+                alpha = (char) (k+'A');
+            }
+            else if(max == maxalpha[k]){
+                flag = 1; //다른 최대값이 존재할 때 그 값이 배열[k]와 같을 땐 중복이 있는 것
+            }
+        }
+        if(flag == 1){
+            System.out.println("?");
+        }
+        else{
+            System.out.println(alpha);
+        }
+    }
+}
