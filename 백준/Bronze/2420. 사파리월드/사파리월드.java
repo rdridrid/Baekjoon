62af32db657b018ea753ffea5bb185a119e7c178
rdridrid <55067985+rdridrid@users.noreply.github.com>
2023-02-27 11:45:11 +0900

    [Bronze V] Title: 사파리월드, Time: 124 ms, Memory: 14108 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long N = Long.parseLong(st.nextToken());
+        long M = Long.parseLong(st.nextToken());
+        long c = (N>M) ? N-M: M-N;
+        System.out.println(c);
+    }
+}
