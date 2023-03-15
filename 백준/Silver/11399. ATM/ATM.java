d59a564a8ec4129e32d3f595570ce6ff27b3fdfb
rdridrid <55067985+rdridrid@users.noreply.github.com>
2023-03-15 13:35:16 +0900

    [Silver IV] Title: ATM, Time: 148 ms, Memory: 14420 KB -BaekjoonHub

---
+++ 백준/Silver/11399. ATM/ATM.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+//선택 정렬
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine()); //1부터 N까지
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int arr[] = new int[N];
+        int sum = 0;
+        int temp = 0;
+        for(int i=0;i<N;i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        //정렬
+        SelectedSort(arr);
+        //
+        for(int i=0;i<N;i++){
+            temp+=arr[i];
+            sum+=temp;
+        }
+        System.out.println(sum);
+    }
+    static void SelectedSort(int[] arr){
+        for(int i=0;i<arr.length-1;i++){
+            int min = i;
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[j]<arr[min]){
+                    min = j;
+                }
+            }
+            int temp2 = arr[min];
+            arr[min] = arr[i];
+            arr[i] = temp2;
+        }
+    }
+}
