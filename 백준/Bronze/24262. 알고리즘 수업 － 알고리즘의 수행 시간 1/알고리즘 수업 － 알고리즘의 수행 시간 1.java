9c2663da7db4d043d587c19ecd7285efaf8fc6c3
rdridrid <55067985+rdridrid@users.noreply.github.com>
2023-03-22 21:46:03 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 128 ms, Memory: 14152 KB -BaekjoonHub

---
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+//HashMap이용
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        System.out.print(1+"\n"+0);
+    }
+    //MenOfPassion은 입력받은 정수를 계산하여 바로 출력하는 함수로
+    //실행횟수는 반드시 1이다.
+}
