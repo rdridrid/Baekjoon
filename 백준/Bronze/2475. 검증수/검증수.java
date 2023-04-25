3d55298f6ac7a80d9543f08e58b76560b1fc8255
rdridrid <55067985+rdridrid@users.noreply.github.com>
2023-04-25 21:58:00 +0900

    [Bronze V] Title: 검증수, Time: 128 ms, Memory: 14180 KB -BaekjoonHub

--- 백준/Bronze/2475. 검증수/검증수.java
+++ 백준/Bronze/2475. 검증수/검증수.java
@@ -1,13 +1,17 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 public class Main{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
+    public static void main(String args[]) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+        int D = Integer.parseInt(st.nextToken());
+        int E = Integer.parseInt(st.nextToken());
         int sum = 0;
-        int temp = 0;
-        for(int i=0;i<5;i++){
-            temp = sc.nextInt();
-            sum = sum+temp*temp;
-        }
+        sum = A*A+B*B+C*C+D*D+E*E;
         System.out.println(sum%10);
+
     }
 }
