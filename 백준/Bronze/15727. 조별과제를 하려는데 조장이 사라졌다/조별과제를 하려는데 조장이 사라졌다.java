d89420e090ffc49d1ab6e1c34dd7b0a00f0a0dde
rdridrid <55067985+rdridrid@users.noreply.github.com>
2023-04-27 21:59:31 +0900

    [Bronze V] Title: 조별과제를 하려는데 조장이 사라졌다, Time: 124 ms, Memory: 14192 KB -BaekjoonHub

---
+++ 백준/Bronze/15727. 조별과제를 하려는데 조장이 사라졌다/조별과제를 하려는데 조장이 사라졌다.java
@@ -0,0 +1,9 @@
+import java.io.*;
+public class Main{
+    public static void main(String args[]) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        N+=4;
+        System.out.println(N/5);
+    }
+}
